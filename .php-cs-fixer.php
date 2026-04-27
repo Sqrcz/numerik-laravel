1b72d85769057d8bdba7a76c3b41d165ad7e47cb
Sqrcz <naorniakowski@slashlab.pl>
2026-04-27 22:35:18 +0200

    chore: add tooling configs and skeleton directory structure (#4)

---
+++ .php-cs-fixer.php
@@ -0,0 +1,15 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => true,
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
